e3505e5912821c53a5f0777274d55a7e77246ba3
Julius Villamayor <juliusvillamayor@gmail.com>
2020-01-07 13:33:18 -0800

    Style slider

--- src/components/Palette.js
+++ src/components/Palette.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
-import './Palette.css'
 import ColorBox from './ColorBox'
 import Slider from 'rc-slider'
 import 'rc-slider/assets/index.css'
+import './Palette.css'
 
 export default class Palette extends Component {
   state = {
@@ -16,13 +16,16 @@ export default class Palette extends Component {
   render() {
     return (
       <div className='Palette'>
-        <Slider
-          defaultValue={this.state.level}
-          min={100}
-          max={900}
-          onChange={this.changeLevel}
-          step={100}
-        />
+        <div className='slider'>
+          <Slider
+            defaultValue={this.state.level}
+            min={100}
+            max={900}
+            onChange={this.changeLevel}
+            step={100}
+            // handleStyle={{ backgroundColor: 'green', outline: 'none' }}
+          />
+        </div>
         <div className='Palette-colors'>
           {this.props.palette.colors[this.state.level].map(c => (
             <ColorBox key={c.hex} {...c} />
