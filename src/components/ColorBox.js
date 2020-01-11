029c23a3cfcd295304365398f8abc9e8540ad192
Julius Villamayor <juliusvillamayor@gmail.com>
2020-01-10 16:40:34 -0800

    Styling text based on luminence

--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import './ColorBox.css'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
 import { Link } from 'react-router-dom'
+import chroma from 'chroma-js'
 
 export default class ColorBox extends Component {
   state = {
@@ -15,6 +16,9 @@ export default class ColorBox extends Component {
   }
 
   render() {
+    // console.log(chroma(this.props.background).luminance())
+    const isDarkColor = chroma(this.props.background).luminance() < 0.04
+    const isLightColor = chroma(this.props.background).luminance() > 0.85
     return (
       <CopyToClipboard
         text={this.props.background.toUpperCase()}
@@ -30,20 +34,28 @@ export default class ColorBox extends Component {
           />
           <div className={`copy-msg ${this.state.isOverlaying && 'show'}`}>
             <h1>Copied</h1>
-            <p>{this.props.background.toUpperCase()}</p>
+            <p className={isLightColor && 'dark-text'}>
+              {this.props.background.toUpperCase()}
+            </p>
           </div>
           <div className='copy-container'>
             <div className='box-content'>
-              <span>{this.props.name}</span>
+              <span className={isDarkColor && 'light-text'}>
+                {this.props.name}
+              </span>
             </div>
-            <button className='copy-button'>Copy</button>
+            <button className={`copy-button ${isLightColor && 'dark-text'}`}>
+              Copy
+            </button>
           </div>
           {this.props.paletteId && (
             <Link
               to={`/palette/${this.props.paletteId}/${this.props.id}`}
               onClick={e => e.stopPropagation()}
             >
-              <span className='see-more'>More</span>
+              <span className={`see-more ${isLightColor && 'dark-text'}`}>
+                More
+              </span>
             </Link>
           )}
         </div>
