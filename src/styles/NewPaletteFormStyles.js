5cb1c2a0af4ea5c0fc1fc5745523379f1ad0aa6c
Julius Villamayor <juliusvillamayor@gmail.com>
2020-01-16 12:59:40 -0800

    Refactor styling

---
+++ src/styles/NewPaletteFormStyles.js
@@ -0,0 +1,59 @@
+import { DRAWER_WIDTH } from '../constants'
+
+const styles = theme => ({
+  root: {
+    display: 'flex'
+  },
+  hide: {
+    display: 'none'
+  },
+  drawer: {
+    width: DRAWER_WIDTH,
+    flexShrink: 0
+  },
+  drawerPaper: {
+    width: DRAWER_WIDTH,
+    display: 'flex',
+    alignItems: 'center'
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: '0 8px',
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end'
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    marginLeft: -DRAWER_WIDTH,
+    height: 'calc(100vh - 64px)'
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen
+    }),
+    marginLeft: 0
+  },
+  container: {
+    width: '90%',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttons: {
+    width: '100%'
+  },
+  button: {
+    width: '50%'
+  }
+})
+
+export default styles
---
+++ src/styles/PaletteFormNavStyles.js
@@ -0,0 +1,39 @@
+import { DRAWER_WIDTH } from '../constants'
+
+const styles = theme => ({
+  root: {
+    display: 'flex'
+  },
+  appBar: {
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    height: '64px',
+    alignItems: 'center'
+  },
+  appBarShift: {
+    width: `calc(100% - ${DRAWER_WIDTH}px)`,
+    marginLeft: DRAWER_WIDTH,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  },
+  menuButton: {
+    marginLeft: 12,
+    marginRight: 20
+  },
+  navBtns: {
+    marginRight: '1rem',
+    '& a': {
+      textDecoration: 'none'
+    }
+  },
+  button: {
+    margin: '0 0.5rem'
+  }
+})
+export default styles
