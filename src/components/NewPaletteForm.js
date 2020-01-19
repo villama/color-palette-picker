1a59d0f9c89c8b16cf409dfd10c50205c6d974a5
Julius Villamayor <juliusvillamayor@gmail.com>
2020-01-19 11:41:52 -0800

    Add min distance to drag for draggable

--- src/components/NewPaletteForm.js
+++ src/components/NewPaletteForm.js
@@ -161,6 +161,7 @@ class NewPaletteForm extends Component {
             deleteColor={this.deleteColor}
             axis='xy'
             onSortEnd={this.onSortEnd}
+            distance={10}
           />
         </main>
       </div>
